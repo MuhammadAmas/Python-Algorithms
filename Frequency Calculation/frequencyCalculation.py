2b68872b7d865981cbf4600434d112da1d5bb118
MuhammadAmas <amaswaseem@gmail.com>
2022-08-30 00:31:02 +0500

    Updated

---
+++ Frequency Calculation/frequencyCalculation.py
@@ -0,0 +1,19 @@
+#opening text file.
+file=open("text.txt","r")
+read=file.read()
+
+#Creating a list of 256 elements
+list=[]
+for a in range (256):
+    list.append(0)
+
+#Converting Characters into ASCII codes.
+for i in read:
+    convert=ord(i)
+    list[convert] += 1
+
+#Converting ASCII codes into characters
+for b in range (256):
+    if list[b] != 0:
+        reconvert=chr(b)
+        print (reconvert, list[b])
---
+++ Rock Paper Scissor/rockPaperScissor.py
@@ -0,0 +1,52 @@
+import random
+
+#Evaluating results
+def gamewin (user , comp):
+    if comp == user:
+        print ("This game is a tie")
+    
+    elif comp == 'r':
+        if user == 'p':
+            return False
+        elif user == 's':
+            return True
+
+    elif comp == 'p':
+        if user == 's':
+            return False
+        elif user == 'r':
+            return True
+
+    elif comp == 's':
+        if user == 'r':
+            return False
+        elif user == 'p':
+            return True
+
+
+user = input("Your Turn: Rock(r) Paper(p) or Scissor(s)?")
+
+#Computer's choice
+rand=random.randint(1,3)
+if rand == 1:
+    comp = 'r'
+elif rand == 2:
+    comp = 'p'
+else:
+    comp = 's'
+
+
+final = gamewin(comp, user)
+#Printing computer's choice
+print(f"Computer chose {comp}")
+#Printing user's choice
+print(f"You chose {user}")
+
+
+#Printing results
+if final == None:
+    print("This game is a tie")
+elif final== True:
+    print("you win")
+else:
+    print("you lose")
